c36b70323ff4a30d6c879072ff16ba207c1c7d20
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-08-11 17:49:46 +0300

    Add Complete function

--- from_marina.py
+++ from_marina.py
@@ -10,7 +10,6 @@ bot = telebot.TeleBot(TOKEN)
 
 wikipedia.set_lang("ru")
 
-
 markup = types.ReplyKeyboardMarkup(row_width=3)
 button1 = types.KeyboardButton("Посмотреть список дел")
 button2 = types.KeyboardButton("Создать задачу")
@@ -19,7 +18,6 @@ button4 = types.KeyboardButton("Справка")
 button5 = types.KeyboardButton("О программе")
 button6 = types.KeyboardButton("Выйти")
 markup.add(button1, button2, button3, button4, button5, button6)
-
 
 if not os.path.exists('data.json'):
     f.write_to_file({})
@@ -66,16 +64,22 @@ def handle_message(msg):
 
         list_markup = types.ReplyKeyboardMarkup(row_width=3)
         edit_button = types.KeyboardButton("Редактировать")
+        completed_button = types.KeyboardButton("Выполнено")
         delete_button = types.KeyboardButton("Удалить")
         cancel_button = types.KeyboardButton("Назад")
-        list_markup.add(edit_button, delete_button, cancel_button)
+        list_markup.add(edit_button, completed_button,
+                        delete_button, cancel_button)
         bot.send_message(chat_id, "Выберите действие",
                          reply_markup=list_markup)
-
     if msg.text == "Редактировать":
         bot.send_message(
             chat_id, 'Введите номер задачи которую необходимо отредактивароть')
         bot.register_next_step_handler(msg, edit_todo_from_list)
+    ###
+    if msg.text == "Выполнено":
+        bot.send_message(chat_id, 'Введите номер задачи которая выполнена')
+        bot.register_next_step_handler(msg, completed_todo_from_list)
+    ###
 
     if msg.text == "Удалить":
         bot.send_message(
@@ -117,25 +121,47 @@ def handle_message(msg):
 
 def add_todo_into_list(message):
     chat_id = str(message.chat.id)
-
     json_data = f.read_from_file()
-
     user_todo = json_data.get(chat_id, [])
-
     user_todo.append(message.text)
     json_data[chat_id] = user_todo
-
     f.write_to_file(json_data)
-
     bot.send_message(chat_id, 'Задача добавлена', reply_markup=markup)
+
+###
+
+
+def completed_todo_from_list(message):
+    chat_id = str(message.chat.id)
+    json_data = f.read_from_file()
+    user_todo = json_data.get(chat_id, [])
+    try:
+        todo_index = int(message.text)
+    except ValueError:
+        bot.send_message(chat_id, "Введи нормальное число!")
+        return
+
+    if todo_index < 0 or todo_index > len(user_todo):
+        bot.send_message(chat_id, "Введи нормальное число! ")
+        return
+
+    def completed_todo(msg):
+        # user_todo[todo_index] = msg.text
+        user_todo[todo_index] = ('~'+user_todo[todo_index]+'~')
+        json_data[chat_id] = user_todo
+        f.write_to_file(json_data)
+        bot.send_message(
+            chat_id, "Я отметил задачу как выполненой", reply_markup=markup)
+
+    bot.send_message(chat_id, '~'+str(user_todo)+'~', parse_mode='MarkdownV2')
+    bot.register_next_step_handler(message, completed_todo)
+###
 
 
 def delete_todo_from_list(message):
     chat_id = str(message.chat.id)
     json_data = f.read_from_file()
-
     user_todo = json_data.get(chat_id, [])
-
     try:
         todo_index = int(message.text)
     except ValueError:
@@ -149,7 +175,6 @@ def delete_todo_from_list(message):
     del user_todo[todo_index]
     json_data[chat_id] = user_todo
     f.write_to_file(json_data)
-
     bot.send_message(chat_id, 'Задача удалена')
 
 
@@ -206,7 +231,8 @@ def getwiki(message):
         bot.send_message(chat_id, wikitext2, reply_markup=markup)
     # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
     except Exception as e:
-        bot.send_message(chat_id, 'В энциклопедии нет информации об этом', reply_markup=markup) 
+        bot.send_message(
+            chat_id, 'В энциклопедии нет информации об этом', reply_markup=markup)
 
 
 print('server started')
